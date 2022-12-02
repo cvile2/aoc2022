7177902b782d472300b6cbaf3bd4e82a844e482c
Chris Vile <c.j.vile@gmail.com>
2022-12-02 11:00:20 +0000

    constexpr

--- aoc_day2.cpp
+++ aoc_day2.cpp
@@ -1,17 +1,17 @@
 #include <fstream>
 #include <iostream>
-#include <map>
 #include <array>
-static constexpr std::array<long, 3> score {{ 1,2, 3}}; //score per shape
-static constexpr std::array<std::pair<char,char>, 3> choiceWL {{ {'B','C'},  {'C','A'}, {'A','B'} }}; 
+static constexpr std::array<long, 3> score {{ 1, 2, 3}}; //score per shape
+static constexpr std::array<std::pair<long,long>, 3> choiceWL {{ {1,2},  {2,0}, {0,1} }}; 
 int main() {
     std::ifstream f{"AOC2.txt"};
     std::string s;
     long score1, score2 = 0;
     while(std::getline(f,s)) { //each line is is e.g. "A Z"
-        int c2 = s[2] - ('X'); //convert X to 0 etc
-        score1 += score.at(c2-'A') + (c2 == s[0] ? /*draw*/ 3 : (choiceWL.at(s[0] - 'A').first == c2  ? /*win*/ 6 : 0 /*loose*/));
-        score2 += (c2 == 'A' ? score.at(choiceWL.at(s[0] - 'A').first-'A') : ( c2=='B' ? score.at(s[0]-'A') + 3 : score.at(choiceWL.at(s[0] - 'A').second-'A') + 6));
+        long c1 = s[0] - ('A'); //convert A to 0 etc
+        long c2 = s[2] - ('X'); //convert X to 0 etc
+        score1 += score.at(c2) + (c2 == c1 ? /*draw*/ 3 : (choiceWL.at(c1).first == c2  ? /*win*/ 6 : 0 /*loose*/));
+        score2 += (c2 == 0 /*WantWin*/ ? score.at(choiceWL.at(c1).first) : ( c2==1 /*WantDraw*/? score.at(c1) + 3 : /*WantLoose*/score.at(choiceWL.at(c1).second) + 6));
     }
     std::cout << score1 << "," << score2;
 }
