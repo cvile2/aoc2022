c5f184382fbaa7a012b5438b5426ef4224ccfb7d
Chris Vile <c.j.vile@gmail.com>
2022-12-03 18:03:43 +0000

    AOC1

---
+++ aoc_day1.cpp
@@ -0,0 +1,29 @@
+//https://adventofcode.com/2022/day/1
+#include <fstream>
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    std::ifstream f{"AOC1.txt"}; 
+    std::string s;
+    long seen = 0; //part1
+    priority_queue<long> cals; //part2
+    while(std::getline(f,s)) {
+        if (s=="")
+        {
+            cals.push(seen);
+            seen = 0;
+        }
+        else
+            seen += atol(s.c_str());
+    }
+    long total, max = cals.top();
+    cals.pop();
+    total += cals.top();
+    cals.pop();
+    total += cals.top();
+
+    std::cout << max << "," << total << endl;
+    return total;
+}
