6a5d8a0348c432294e306f83dc140db126c35918
Chris Vile <chris.vile@gmail.com>
2022-12-30 11:28:40 +0000

    AOC14 p2

--- aoc_day14.cpp
+++ aoc_day14.cpp
@@ -4,13 +4,13 @@
 #include <vector>
 #include <string>
 using namespace std;
-
 using tPos = pair<size_t,size_t>;
+constexpr auto NUMBERS = "0123456789";
 
 class Mine {
-    static int getNum(const string& line, size_t& pos) {
-        auto l = line.find_first_of("0123456789", pos);
-        auto r = line.find_first_not_of("0123456789", l);
+    static int get_num(const string& line, size_t& pos) {
+        auto l = line.find_first_of(NUMBERS, pos);
+        auto r = line.find_first_not_of(NUMBERS, l);
         pos = max(l,r);
         return stoi(line.substr(l, r));
     }
@@ -20,7 +20,7 @@ class Mine {
         maxx= 500, 
         maxy= 0;
 
-    void linefill(auto& grid, tPos from, const tPos& to) {
+    void linefill(auto& grid, tPos from, const tPos& to) const {
         int xdf = to.first - from.first;
         int ydf = to.second - from.second;         
         bool exit = false;
@@ -35,15 +35,16 @@ class Mine {
     }
 
     void print_grid(const auto& grid) const {
-        cout << "Grid:\n";
+        cout << "Grid: Limits: " << minx << "," 
+            << miny << " " << maxx << "," << maxy << endl;
+        
         for( size_t i = 0; i < grid.size(); ++i )
             cout << i << ":" << grid[i] << endl;
     }
 
-    bool can_fall(const auto& grid, const tPos& p ) const{
-        if (p.first<0 || p.first>=maxx || p.second>=maxy) {
+    bool can_fall(const auto& grid, const tPos& p ) const {
+        if (p.first<0 || p.first>=maxx || p.second>=maxy)
             return true; //void
-        }
         return (grid.at(p.second).at(p.first)  == '.');
     }
 
@@ -52,7 +53,6 @@ class Mine {
             //cout << "VOID: " << p.first << "," << p.second << endl;
             return true; //void
         }
-        
         if(can_fall(grid, {p.first, p.second+1})) //DOWN
             return fall(grid, {p.first, p.second+1}); 
         else if(can_fall(grid, {p.first-1, p.second+1}))
@@ -62,19 +62,17 @@ class Mine {
         else
             grid.at(p.second).at(p.first) = 'o'; //ALL blocked - settled
 
-        //print_grid(grid);
         return false; //not void
     };
-public:
 
-    int process(ifstream&& f) { 
+    auto parse_file(ifstream& f) {
         string line;
+        //Parse file to vector of positions
         vector<vector<tPos>> paths;
-        
         while(getline(f,line)) {
             paths.push_back(vector<tPos>());
             for (size_t pos = 0; pos < string::npos;) {
-                tPos c = tPos{getNum(line, pos),getNum(line, pos)};
+                tPos c = tPos{get_num(line, pos),get_num(line, pos)};
                 minx = min(minx, c.first);
                 miny = min(miny, c.second);
                 maxx = max(maxx, c.first);
@@ -82,11 +80,26 @@ public:
                 paths.back().emplace_back(std::move(c));
             }
         }
-        maxx = maxx-minx+1;
-        maxy = maxy-miny+1;
-        //cout << "\nLimits: " << minx << "," << miny << " " << maxx << "," << maxy << " | " << endl;
-        vector<string> grid(maxy, string(maxx, '.'));
+        return paths;
+    }
+public:
+
+    int process(ifstream&& f, bool part2) { 
+        auto paths = parse_file(f);
+
+        //Add part 2 solid floor
+        if (part2) {
+            maxy+=2;
+            maxx=500+(maxy)+1;
+            minx=500-(maxy)-1;
+            paths.push_back(vector<tPos>{{minx,maxy},{maxx,maxy}});
+        }
         
+        //Fill the grid solid walls
+        maxx = maxx-minx+1; //recalibrate to 0 to n grid
+        maxy = maxy-miny+1;
+        
+        vector<string> grid(maxy, string(maxx, '.'));
         for( size_t i = 0; i < paths.size(); ++i )  {
             auto& from = paths[i][0];
             for( size_t j = 1; j < paths[i].size(); ++j) {
@@ -96,7 +109,8 @@ public:
             }
         }
         int sandCount = 0;
-        while(!fall(grid, { 500 - minx, 0 })) {
+        while((can_fall(grid, { 500-minx,0 }) 
+            && !fall(grid, { 500-minx,0 }))) {
             sandCount++;
         }
         return sandCount; 
@@ -104,7 +118,10 @@ public:
 };
 
 int main() {
-    cout << "part 1 example:" << Mine{}.process(ifstream("AOC14_example.txt")) << endl;
-    cout << "part 1:" << Mine{}.process(ifstream("AOC14.txt")) << endl;
+    cout << "part 1 example:" << Mine{}.process(ifstream("AOC14_example.txt"), false) << endl;
+    cout << "part 1:" << Mine{}.process(ifstream("AOC14.txt"), false) << endl;
+    cout << "part 2 example:" << Mine{}.process(ifstream("AOC14_example.txt"), true) << endl;
+    cout << "part 2:" << Mine{}.process(ifstream("AOC14.txt"), true) << endl;
+    
     return 0;
 }
